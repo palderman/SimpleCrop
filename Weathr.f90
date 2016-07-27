6ca1f7c3ee0b7fdc8aacfd4edd72dfb29c2c1271
Phillip D. Alderman <phillip.d.alderman@gmail.com>
2016-07-27 10:59:23 -0500

    Revised Weathr for f90

--- Weathr.f90
+++ Weathr.f90
@@ -1,63 +1,46 @@
-************************************************************************
-************************************************************************
-*     SUBROUTINE WEATHR - Reads daily weather data from file
-************************************************************************
-*
-*     LIST OF VARIABLES
-*
-*     DATE = date of weather record (YYDDD)
-*     DYN  = dynamic control variable
-*     PAR  = photosynthetically active radiation (MJ/m2/d)
-*     RAIN = daily rainfall (mm)
-*     SRAD = daily solar radiation (MJ/m2/d)
-*     TMAX = daily maximum temperature (Celsius)
-*     TMIN = daily minimum temperature (Celsius)
-*
-***********************************************************************
-      SUBROUTINE WEATHR(SRAD,TMAX,TMIN,RAIN,PAR,DYN)
+!************************************************************************
+!************************************************************************
+!*     subroutine reat_wth Reads daily weather data from file
+!************************************************************************
+!*
+!*     LIST OF VARIABLES
+!*
+!*     DATE = date of weather record (YYDDD)
+!*     DYN  = dynamic control variable
+!*     PAR  = photosynthetically active radiation (MJ/m2/d)
+!*     RAIN = daily rainfall (mm)
+!*     SRAD = daily solar radiation (MJ/m2/d)
+!*     TMAX = daily maximum temperature (Celsius)
+!*     TMIN = daily minimum temperature (Celsius)
+!*
+!***********************************************************************
+subroutine read_wth(srad,tmax,tmin,rain,par)
 
-!-----------------------------------------------------------------------  
-      IMPLICIT NONE 
-      SAVE
-      REAL SRAD,TMAX,TMIN,RAIN,PAR
-      INTEGER DATE
-      CHARACTER*10 DYN
+  implicit none
 
-!************************************************************************
-!************************************************************************
-!     INITIALIZATION
-!************************************************************************
-      IF (INDEX(DYN,'INITIAL') .NE. 0) THEN
-!************************************************************************
-        OPEN (4,FILE='WEATHER.INP',STATUS='UNKNOWN')  
-      
-!************************************************************************
-!************************************************************************
-!     RATE CALCULATIONS
-!************************************************************************
-      ELSEIF (INDEX(DYN,'RATE') .NE. 0) THEN
-!************************************************************************
-C     Loop to compute data for one year. Climatic data of the year 1987,
-C     for Gainesville, Florida, were used as verification of the module.
+  real SRAD,TMAX,TMIN,RAIN,PAR
 
-        READ(4,20) DATE,SRAD,TMAX,TMIN,RAIN,PAR
-   20   FORMAT(I5,2X,F4.1,2X,F4.1,2X,F4.1,F6.1,14X,F4.1)
-      
-        PAR = 0.5 *  SRAD   ! Par is defined as 50% of SRAD
+  read(4,'(7X,F4.1,2X,F4.1,2X,F4.1,F6.1,14X,F4.1)') srad,tmax,tmin,rain,par
+  par = 0.5 *  srad         ! Par is defined as 50% of SRAD
 
-!************************************************************************
-!************************************************************************
-      ELSEIF (INDEX(DYN,'CLOSE') .NE. 0) THEN
-!************************************************************************
-        CLOSE(4) 
+  return
+end subroutine read_wth
 
-!************************************************************************
-!************************************************************************
-!     End of dynamic 'IF' construct
-!************************************************************************
-      ENDIF
-!************************************************************************
-      RETURN
-      END SUBROUTINE WEATHR
-************************************************************************
-************************************************************************
+subroutine  open_wth(filename)
+
+  implicit none
+
+  character(len=*) filename
+
+  open(unit=4,file=filename)
+
+end subroutine open_wth
+
+
+subroutine close_wth()
+
+  implicit none
+
+  close(4)
+
+end subroutine close_wth
